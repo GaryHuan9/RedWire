a290fb938044c036351a02a020ba7c9fc917bc5c
CXRedix <1135058679@qq.com>
2021-07-12 10:41:01 +0800

    Added Information section and removed TestUI

--- RedWire/src/Interface/Information.cpp
+++ RedWire/src/Interface/Information.cpp
@@ -1,6 +1,9 @@
 #include "Toolbox.h"
 #include "Information.h"
 
+#include "../Application.h"
+#include "../Core/Grid.h"
+
 #include "imgui.h"
 
 using namespace RedWire;
@@ -11,4 +14,31 @@ Information::Information(Toolbox& toolbox) : Section(toolbox)
 void Information::show()
 {
 	if (!ImGui::CollapsingHeader("Information")) return;
+
+	// == calculate FPS ==
+
+	if (currentInterval >= FPS_UPDATE_INTERVAL)
+	{
+		lastAverageFPS = (float)frameCount / currentInterval;
+
+		frameCount = 0u;
+		currentInterval = 0.f;
+	}
+
+	frameCount++;
+
+	auto& application = toolbox.application;
+
+	currentInterval += application.getDeltaTime().asSeconds();
+
+	// == draw text ==
+	const std::string tileCountString = std::string("Tile Count: ") + std::to_string(application.grid->getTileCount());
+	const std::string wireCountString = std::string("Wire Count: ") + std::to_string(application.grid->getWireCount());
+	const std::string gateCountString = std::string("Gate Count: ") + std::to_string(application.grid->getGateCount());
+	const std::string FPSString       = std::string("FPS: ") + std::to_string(lastAverageFPS);
+
+	ImGui::Text(tileCountString.c_str());
+	ImGui::Text(wireCountString.c_str());
+	ImGui::Text(gateCountString.c_str());
+	ImGui::Text(FPSString.c_str());
 }
--- RedWire/src/Interface/Information.h
+++ RedWire/src/Interface/Information.h
@@ -11,5 +11,12 @@ namespace RedWire
 		Information(Toolbox& toolbox);
 
 		virtual void show() override;
+
+	private:
+		const float FPS_UPDATE_INTERVAL = 1.f;
+
+		float currentInterval = 0.f;
+		float lastAverageFPS = 0.f;
+		int frameCount = 0;
 	};
 }
